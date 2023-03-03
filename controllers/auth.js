f35159a2c7cf41d3856f4a20569cf86b56eee34a
OlaOluwalekan <olalekanbello534@gmail.com>
2023-03-03 14:02:31 +0100

    secure removed

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,7 +44,6 @@ const loginUser = async (req, res, next) => {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24,
         sameSite: 'None',
-        secure,
       })
       .status(200)
       .json({
