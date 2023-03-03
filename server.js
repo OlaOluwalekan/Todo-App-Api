036bf6dc50559ff0c1a222c9049c52be312cdd71
OlaOluwalekan <olalekanbello534@gmail.com>
2023-03-03 12:31:00 +0100

    cors modified again

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const usersRoutes = require('./routes/users')
 const todosRoutes = require('./routes/todos')
 
 // MIDDLEWARES
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:5500',
+    credentials: true,
+  })
+)
 app.use(cookieParser())
 app.use(express.json())
 
