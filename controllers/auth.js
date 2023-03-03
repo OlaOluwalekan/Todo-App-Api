fe2a17ac70d0c8fb098d5f88b134788f554cf970
OlaOluwalekan <olalekanbello534@gmail.com>
2023-03-03 13:38:28 +0100

    samesite adjusted

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,7 +47,7 @@ const loginUser = async (req, res, next) => {
           httpOnly: true,
           maxAge: 1000 * 60 * 60 * 24,
         },
-        'sameSite=None'
+        'samesite=none'
       )
       .status(200)
       .json({
