ce7a570a0813b08af0f86f2f90a6ad530b1f0fb9
OlaOluwalekan <81148417+OlaOluwalekan@users.noreply.github.com>
2023-03-12 10:05:08 +0100

    Update cors.js
    update vite port

--- utils/cors.js
+++ utils/cors.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   'https://app-makelist.netlify.app',
   'http://127.0.0.1:5500',
   'http://localhost:5500',
-  'http://localhost:5173/',
+  'http://localhost:5173',
 ]
 
 const corsOptions = {
